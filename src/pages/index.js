37b6930f9bc23a8fffdf26902797504904c89c08
Emiliano <emiliano@MacBook-Pro-de-Emiliano.local>
2021-01-02 18:03:09 -0300

    added fontawesome icons and improve main layout with grid

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,15 @@
 import React from "react";
-import { graphql } from "gatsby"
+import { graphql } from "gatsby";
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faLinkedinIn,
+  faGithub,
+  faYoutube,
+} from '@fortawesome/free-brands-svg-icons';
+import {
+  faEnvelope
+} from '@fortawesome/free-solid-svg-icons';
 
 import P5 from '../p5/p5';
 import Img from "gatsby-image";
@@ -11,7 +21,7 @@ export default function Home({ data }) {
 
     <P5 />
 
-    <div class="container mx-auto my-14 px-44 text-center text-primary text-4xl font-mono">
+    <div className="container mx-auto my-14 px-6 md:px-12 lg:px-12 text-center text-primary text-4xl font-mono">
       <p>Hello, my name is <strong>Emiliano</strong>.</p>
       <br />
       <p>I'm a Front End Developer working remotely from Ciudad de Córdoba, Argentina.</p> 
@@ -31,7 +41,7 @@ export default function Home({ data }) {
       Interested in hire me or doing a project together?
       </p>
       <br />
-			<button class="uppercase px-8 py-2 border border-green-600 text-green-600 max-w-max shadow-sm hover:shadow-md">EMAIL ME</button>
+			<button className="uppercase px-8 py-2 border border-green-600 text-green-600 max-w-max shadow-sm hover:shadow-md">EMAIL ME</button>
       <br />
       <br />
       <br />
@@ -41,58 +51,148 @@ export default function Home({ data }) {
       </p>
       <br />
       
-      <div className="flex space-x-4">
-        <div class="max-w-sm rounded overflow-hidden shadow-lg flex-1">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 justify-center content-center">
+        <div className="max-w-sm rounded overflow-hidden shadow-lg flex-1 m-auto">
         <Img
           fluid={data.file.childImageSharp.fluid}
           alt="Emiliano Lucero"
         />
-          <div class="px-6 py-4">
-            <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
-            <p class="text-gray-700 text-base">
+          <div className="px-6 py-4">
+            <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
+            <p className="text-gray-700 text-base">
               Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
             </p>
           </div>
-          <div class="px-6 py-4">
-            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#photography</span>
-            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#travel</span>
-            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">#winter</span>
+          <div className="px-6 py-4">
+            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#photography</span>
+            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#travel</span>
+            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">#winter</span>
           </div>
         </div>
 
-        <div class="max-w-sm rounded overflow-hidden shadow-lg flex-1">
+        <div className="max-w-sm rounded overflow-hidden shadow-lg flex-1 m-auto">
         <Img
           fluid={data.file.childImageSharp.fluid}
           alt="Emiliano Lucero"
         />
-          <div class="px-6 py-4">
-            <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
-            <p class="text-gray-700 text-base">
+          <div className="px-6 py-4">
+            <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
+            <p className="text-gray-700 text-base">
               Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
             </p>
           </div>
-          <div class="px-6 py-4">
-            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#photography</span>
-            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#travel</span>
-            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">#winter</span>
+          <div className="px-6 py-4">
+            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#photography</span>
+            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#travel</span>
+            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">#winter</span>
           </div>
         </div>
 
-        <div class="max-w-sm rounded overflow-hidden shadow-lg flex-1">
+        <div className="max-w-sm rounded overflow-hidden shadow-lg flex-1 m-auto">
         <Img
           fluid={data.file.childImageSharp.fluid}
           alt="Emiliano Lucero"
         />
-          <div class="px-6 py-4">
-            <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
-            <p class="text-gray-700 text-base">
+          <div className="px-6 py-4">
+            <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
+            <p className="text-gray-700 text-base">
               Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
             </p>
           </div>
-          <div class="px-6 py-4">
-            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#photography</span>
-            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#travel</span>
-            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">#winter</span>
+          <div className="px-6 py-4">
+            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#photography</span>
+            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#travel</span>
+            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">#winter</span>
+          </div>
+        </div>
+
+        <div className="max-w-sm rounded overflow-hidden shadow-lg flex-1 m-auto">
+        <Img
+          fluid={data.file.childImageSharp.fluid}
+          alt="Emiliano Lucero"
+        />
+          <div className="px-6 py-4">
+            <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
+            <p className="text-gray-700 text-base">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+            </p>
+          </div>
+          <div className="px-6 py-4">
+            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#photography</span>
+            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#travel</span>
+            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">#winter</span>
+          </div>
+        </div>
+
+        <div className="max-w-sm rounded overflow-hidden shadow-lg flex-1 m-auto">
+        <Img
+          fluid={data.file.childImageSharp.fluid}
+          alt="Emiliano Lucero"
+        />
+          <div className="px-6 py-4">
+            <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
+            <p className="text-gray-700 text-base">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+            </p>
+          </div>
+          <div className="px-6 py-4">
+            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#photography</span>
+            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#travel</span>
+            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">#winter</span>
+          </div>
+        </div>
+
+        <div className="max-w-sm rounded overflow-hidden shadow-lg flex-1 m-auto">
+        <Img
+          fluid={data.file.childImageSharp.fluid}
+          alt="Emiliano Lucero"
+        />
+          <div className="px-6 py-4">
+            <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
+            <p className="text-gray-700 text-base">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+            </p>
+          </div>
+          <div className="px-6 py-4">
+            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#photography</span>
+            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#travel</span>
+            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">#winter</span>
+          </div>
+        </div>
+
+        <div className="max-w-sm rounded overflow-hidden shadow-lg flex-1 m-auto">
+        <Img
+          fluid={data.file.childImageSharp.fluid}
+          alt="Emiliano Lucero"
+        />
+          <div className="px-6 py-4">
+            <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
+            <p className="text-gray-700 text-base">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+            </p>
+          </div>
+          <div className="px-6 py-4">
+            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#photography</span>
+            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#travel</span>
+            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">#winter</span>
+          </div>
+        </div>
+
+        <div className="max-w-sm rounded overflow-hidden shadow-lg flex-1 m-auto">
+        <Img
+          fluid={data.file.childImageSharp.fluid}
+          alt="Emiliano Lucero"
+        />
+          <div className="px-6 py-4">
+            <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
+            <p className="text-gray-700 text-base">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+            </p>
+          </div>
+          <div className="px-6 py-4">
+            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#photography</span>
+            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#travel</span>
+            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">#winter</span>
           </div>
         </div>
 
@@ -100,16 +200,15 @@ export default function Home({ data }) {
         
       </div>
 
-
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
       <div>
-        <p>Contact me</p>
+        <p className="my-20">Contact me</p>
 
-
+        <div className="grid grid-cols-1 md:grid-cols-4 gap-2 mx-1 md:mx-16 lg:mx-56">
+          <FontAwesomeIcon className="m-auto" icon={faLinkedinIn} size="2x" />
+          <FontAwesomeIcon className="m-auto" icon={faGithub} size="2x" />
+          <FontAwesomeIcon className="m-auto" icon={faEnvelope} size="2x" />
+          <FontAwesomeIcon className="m-auto" icon={faYoutube} size="2x" />
+        </div>
       </div>
     </div>
 
