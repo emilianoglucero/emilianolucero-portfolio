5f11855e71dfca219f2bf94bc0b8f1994e2c7f7f
emiliano lucero <emilianoglucero@gmail.com>
2024-09-22 18:01:53 -0300

    feat: Update gatsby-config.js to use gatsby-plugin-google-gtag for Google Analytics tracking

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,9 +8,6 @@
 require("dotenv").config({
   path: `.env.${process.env.NODE_ENV}`,
 })
-// require("dotenv").config({
-//   path: `.env.${process.env.NODE_ENV || "development"}`,
-// });
 
 module.exports = {
   /* Your site config here */
@@ -18,30 +15,9 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-gtag`,
       options: {
-        trackingIds: [
-          process.env.GA_TRACKING_ID, // Google Analytics / GA
-          // "AW-CONVERSION_ID",  Google Ads / Adwords / AW
-          // "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
-        ],
-        // This object gets passed directly to the gtag config command
-        // This config will be shared across all trackingIds
-        // gtagConfig: {
-        //   optimize_id: "OPT_CONTAINER_ID",
-        //   anonymize_ip: true,
-        //   cookie_expires: 0,
-        // },
-        // This object is used for configuration specific to this plugin
+        trackingIds: [process.env.GA_TRACKING_ID],
         pluginConfig: {
-          // Puts tracking script in the head instead of the body
-          head: true,
-          // Setting this parameter is also optional
-          // respectDNT: true,
-          // // Avoids sending pageview hits from custom paths
-          // exclude: ["/preview/**", "/do-not-track/me/too/"],
-          // // Defaults to https://www.googletagmanager.com
-          // origin: "YOUR_SELF_HOSTED_ORIGIN",
-          // // Delays processing pageview events on route update (in milliseconds)
-          // delayOnRouteUpdate: 0,
+          head: true, // Ensure the script is in the head
         },
       },
     },
