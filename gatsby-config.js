94ef12dbdd74a1c50a04ecffa2e8f8ce3c2a85c8
emiliano lucero <emilianoglucero@gmail.com>
2024-09-22 16:17:20 -0300

    feat: Update package.json and gatsby-config.js
    Update package.json to remove the "analytics" dependency and add the "gatsby-plugin-google-gtag" dependency.
    Update gatsby-config.js to replace the deprecated "gatsby-plugin-google-analytics" with "gatsby-plugin-google-gtag".

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,9 +4,9 @@
  * See: https://www.gatsbyjs.com/docs/gatsby-config/
  */
 
- //Gatsby settings for the environment variables
+//Gatsby settings for the environment variables
 require("dotenv").config({
-  path: `.env.${process.env.NODE_ENV}`
+  path: `.env.${process.env.NODE_ENV}`,
 })
 // require("dotenv").config({
 //   path: `.env.${process.env.NODE_ENV || "development"}`,
@@ -16,40 +16,61 @@ module.exports = {
   /* Your site config here */
   plugins: [
     {
-      resolve: `gatsby-plugin-google-analytics`,
+      resolve: `gatsby-plugin-google-gtag`,
       options: {
-        // The property ID; the tracking code won't be generated without it
-        trackingId: process.env.GA_TRACKING_ID,
-        // this option places the tracking script into the head of the DOM
-        head: true,
-      }, 
+        trackingIds: [
+          process.env.GA_TRACKING_ID, // Google Analytics / GA
+          // "AW-CONVERSION_ID",  Google Ads / Adwords / AW
+          // "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        // gtagConfig: {
+        //   optimize_id: "OPT_CONTAINER_ID",
+        //   anonymize_ip: true,
+        //   cookie_expires: 0,
+        // },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+          // Setting this parameter is also optional
+          // respectDNT: true,
+          // // Avoids sending pageview hits from custom paths
+          // exclude: ["/preview/**", "/do-not-track/me/too/"],
+          // // Defaults to https://www.googletagmanager.com
+          // origin: "YOUR_SELF_HOSTED_ORIGIN",
+          // // Delays processing pageview events on route update (in milliseconds)
+          // delayOnRouteUpdate: 0,
+        },
+      },
     },
     {
-    resolve: `gatsby-plugin-postcss`,
-    options: {
+      resolve: `gatsby-plugin-postcss`,
+      options: {
         postCssPlugins: [require("tailwindcss")],
-        },
+      },
     },
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'Emiliano Lucero Portfolio',
-        short_name: 'Emiliano Lucero',
-        start_url: '/',
-        background_color: '#f7f0eb',
-        theme_color: '#a2466c',
-        display: 'standalone',
-        icon: 'src/images/favicon.png',
+        name: "Emiliano Lucero Portfolio",
+        short_name: "Emiliano Lucero",
+        start_url: "/",
+        background_color: "#f7f0eb",
+        theme_color: "#a2466c",
+        display: "standalone",
+        icon: "src/images/favicon.png",
       },
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     // 'gatsby-plugin-optimize-svgs',
-      {
-        resolve: `gatsby-source-filesystem`,
-        options: {
-          path: `${__dirname}/src/images/`,
-        },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/images/`,
       },
+    },
   ],
 }
