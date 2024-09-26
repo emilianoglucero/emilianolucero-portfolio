2120f7c7e562db43b394b3f0b76884f9101f948a
emiliano lucero <emilianoglucero@gmail.com>
2024-09-26 09:22:46 -0300

    feat: Add Instagram link to Footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,6 +6,7 @@ import {
   faGithub,
   faYoutube,
   faTwitter,
+  faInstagram,
 } from "@fortawesome/free-brands-svg-icons"
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons"
 import CopyToClipboardButton from "../hooks/useCopyToClipboard"
@@ -14,16 +15,34 @@ export const Footer = () => {
   return (
     <>
       <div>
-        <p className="mt-32 mb-16 leading-normal">
-          I'm always excited to collaborate on new projects or just have a chat.
-          Feel free to reach out to me!
-        </p>
-        <CopyToClipboardButton content="emilianoglucero@gmail.com" />
+        <div>
+          <p className="mt-32 leading-normal mb-11">
+            I'm always excited to collaborate on new projects or just have a
+            chat. Feel free to reach out to me!
+          </p>
+          <CopyToClipboardButton content="emilianoglucero@gmail.com" />
+        </div>
+        <div>
+          <p className="mt-24 leading-normal mb-11">
+            Want to know more about my work experience?
+          </p>
+          <button
+            className="px-8 py-2 text-green-600 uppercase transition duration-300 border border-green-600 shadow-sm max-w-max hover:shadow-md"
+            onClick={() =>
+              window.open(
+                "https://drive.google.com/file/d/1IkGToghTlPxuE47fO8RKe02CeuEjX3dC/view?usp=sharing",
+                "_blank"
+              )
+            }
+          >
+            My Resume
+          </button>
+        </div>
       </div>
       <div>
-        {/* <p className="pt-24 pb-10">Find me on</p> */}
+        <p className="mt-32 mb-11">Find me on:</p>
 
-        <div className="grid grid-cols-1 gap-2 mx-1 my-16 md:grid-cols-5 md:mx-16 lg:mx-56">
+        <div className="grid grid-cols-1 gap-2 mx-1 my-16 md:grid-cols-6 md:mx-16 lg:mx-56">
           <a
             className="transition-transform transform duration-250 hover:rotate-3"
             href="https://www.linkedin.com/in/emilianoglucero/"
@@ -86,6 +105,19 @@ export const Footer = () => {
               aria-label="X"
               className="m-auto"
               icon={faTwitter}
+              size="2x"
+            />
+          </a>
+          <a
+            className="transition-transform transform duration-250 hover:rotate-3"
+            href="https://www.instagram.com/emilianogiannakopoulos/"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <FontAwesomeIcon
+              aria-label="Instagram"
+              className="m-auto"
+              icon={faInstagram}
               size="2x"
             />
           </a>
