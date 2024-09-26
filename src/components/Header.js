b4268624c04da2bd5eaa27fcf3b58a7ab10cd008
emiliano lucero <emilianoglucero@gmail.com>
2024-09-26 09:22:35 -0300

    Update Header.js: improve formatting and update personal description

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,8 @@ export const Header = () => {
   return (
     <div>
       <p className="my-5 leading-normal">
-        Hey there, I'm <span className="font-semibold">Emiliano</span>.<br />
+        Hey there, I'm<span> Emiliano</span>.
+        <br />
         Web Developer turning ideas into memorable digital experiences.
       </p>
       <div className="my-11">
@@ -15,10 +16,13 @@ export const Header = () => {
         />
       </div>
       <p className="leading-normal my-11">
-        As a skilled full-stack developer with 7+ years of experience in the software industry, I bring a keen eye for design to every project, ensuring interfaces that people love and help businesses achieve their goals.
+        As a skilled full-stack developer with 7+ years of experience in the
+        software industry, I bring a keen eye for design to every
+        project, ensuring interfaces that people love and help businesses
+        achieve their goals.
       </p>
-      <p className="mt-24 leading-normal my-11">
-        Personal experiments and some client projects:
+      <p className="mt-24 leading-normal mb-11">
+        Personal experiments and client projects:
       </p>
     </div>
   )
