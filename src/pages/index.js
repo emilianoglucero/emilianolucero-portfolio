abc98adaeedbade73d7e98a4a35798a11134f0a1
emiliano lucero <emilianoglucero@gmail.com>
2024-09-27 11:19:56 -0300

    refactor: Update SEO metadata and og:image in index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -25,8 +25,8 @@ export default function Home() {
     <>
       <P5 />
       <Helmet>
-        <title>Emiliano Lucero Web Dev Portfolio</title>
-        <meta property="og:title" content="Emiliano Lucero Web Dev Portfolio" />
+        <title>Emiliano Lucero the Web Dev</title>
+        <meta property="og:title" content="Emiliano Lucero the Web Dev" />
         <meta
           property="og:description"
           content="A skilled full-stack developer with 7+ years of experience in the software industry"
@@ -37,6 +37,32 @@ export default function Home() {
         />
         <meta property="og:url" content="https://www.emilianolucero.ar/" />
         <meta property="og:type" content="website" />
+
+        {/* Facebook */}
+        <meta property="og:title" content="Emiliano Lucero the Web Dev" />
+        <meta
+          property="og:description"
+          content="A skilled full-stack developer with 7+ years of experience in the software industry"
+        />
+        <meta
+          property="og:image"
+          content="https://storage.googleapis.com/bebeto-pizza-dibuja/og-images/og-image-portfolio.jpg"
+        />
+        <meta property="og:url" content="https://www.emilianolucero.ar/" />
+        <meta property="og:type" content="website" />
+
+        {/* Twitter */}
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://www.emilianolucero.ar/" />
+        <meta property="twitter:title" content="Emiliano Lucero the Web Dev" />
+        <meta
+          property="twitter:description"
+          content="A skilled full-stack developer with 7+ years of experience in the software industry"
+        />
+        <meta
+          property="twitter:image"
+          content="https://storage.googleapis.com/bebeto-pizza-dibuja/og-images/og-image-portfolio.jpg"
+        />
       </Helmet>
 
       <div className="container px-6 mx-auto font-mono text-4xl text-center my-14 md:px-12 lg:px-12 text-primary">
