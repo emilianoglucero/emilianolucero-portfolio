df8e35056714c0f29ec34a9ee0c9c887d6093d6d
emiliano lucero <emilianoglucero@gmail.com>
2024-09-30 16:04:09 -0300

    refactor: Optimize SEO metadata and add animation to intro

--- src/p5/p5.js
+++ src/p5/p5.js
@@ -1,18 +1,26 @@
-import React, { Component } from "react"
-import {loadableP5 as P5Wrapper} from './loadable';
-import Sketch from "./sketch";
+import React from "react"
+import { loadableP5 as P5Wrapper } from "./loadable"
+import Sketch from "./sketch"
+import "./p5.css"
 
-import "./p5.css";
+import gsap from "gsap"
+import useIsomorphicLayoutEffect from "../hooks/use-isomorphic-layout-effect"
 
-export default class App extends Component{
+const P5 = () => {
+  useIsomorphicLayoutEffect(() => {
+    gsap.to(".p5-background", {
+      opacity: 1,
+      delay: 7.5,
+      duration: 1,
+      ease: "power2.inOut",
+    })
+  })
 
-  render() {
-    
-    return (
-      <div className="p5-background">
-        <P5Wrapper sketch={Sketch} />
-      </div>
-
-    )
-  }
+  return (
+    <div className="opacity-0 p5-background">
+      <P5Wrapper sketch={Sketch} />
+    </div>
+  )
 }
+
+export default P5
