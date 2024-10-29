d92e8ad0c13b929609ea06d9126c13b9efaff807
emiliano lucero <emilianoglucero@gmail.com>
2024-10-28 23:48:52 -0300

    Refactor VideoCard and Image components
    This commit refactors the VideoCard and Image components. The VideoCard component is created with the necessary props and renders a video player with a paused overlay image. The Image component is also refactored to use a StaticQuery to fetch images and render them using the GatsbyImageSharpFluid_withWebp plugin.

---
+++ src/components/Image/Image.js
@@ -0,0 +1,36 @@
+import React from "react"
+import { StaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
+
+export const Image = props => (
+  <StaticQuery
+    query={graphql`
+      query {
+        images: allFile {
+          edges {
+            node {
+              relativePath
+              name
+              childImageSharp {
+                fluid {
+                  ...GatsbyImageSharpFluid_withWebp
+                }
+              }
+            }
+          }
+        }
+      }
+    `}
+    render={data => {
+      const image = data.images.edges.find(n => {
+        return n.node.relativePath.includes(props.filename)
+      })
+      if (!image) {
+        return null
+      }
+
+      const imageSizes = image.node.childImageSharp.fluid
+      return <Img alt={props.alt} fluid={imageSizes} style={props.style} />
+    }}
+  />
+)
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,42 +1,42 @@
 import React from "react"
-import { Image } from "../components/Image"
+import VideoCard from "../VideoCard"
 
 export const Projects = ({ info }) => {
+  const colStartClass = `lg:col-start-${info.colStart}`
+  const colSpanClass = `lg:col-span-${info.colSpan}`
+  const rowStartClass = `lg:row-start-${info.rowStart}`
+
   return (
-    <div className="flex flex-col flex-1 max-w-sm m-auto overflow-hidden transition duration-300 transform skew-y-12 rounded shadow-lg my-11 hover:shadow-md hover:opacity-80">
-      <a
-        className="transition duration-250 hover:scale-95"
-        href={info.href}
-        rel="noreferrer"
-        target="_blank"
-      >
-        <Image filename={info.filename} />
-      </a>
+    <div
+      className={`flex flex-col flex-1 m-auto overflow-hidden skew-y-12 rounded shadow-lg my-11 hover:shadow-md hover:bg-primary ${colStartClass} ${colSpanClass} ${rowStartClass}`}
+    >
+      <VideoCard info={info} />
       <div className="flex-grow px-6 py-4">
-        <div className="mb-2 text-xl font-bold">{info.title}</div>
-        <p className="text-base text-gray-700">{info.description}</p>
+        <div className="mb-2 text-xl font-ft88">{info.title}</div>
+        <p
+          className="text-base text-gray-700"
+          style={{
+            textWrap: "balance",
+          }}
+        >
+          {info.description}
+        </p>
       </div>
       <div className="px-6 py-4">
-        {info.tag1 !== "" ? (
+        {info.tag1 && (
           <span className="inline-block px-3 py-1 mr-2 text-sm font-semibold text-gray-700 transition delay-75 bg-gray-200 rounded-full duration-400 hover:bg-transparent">
             #{info.tag1}
           </span>
-        ) : (
-          ""
         )}
-        {info.tag2 !== "" ? (
+        {info.tag2 && (
           <span className="inline-block px-3 py-1 mr-2 text-sm font-semibold text-gray-700 transition delay-75 bg-gray-200 rounded-full duration-400 hover:bg-transparent">
             #{info.tag2}
           </span>
-        ) : (
-          ""
         )}
-        {info.tag3 !== "" ? (
+        {info.tag3 && (
           <span className="inline-block px-3 py-1 text-sm font-semibold text-gray-700 transition delay-75 bg-gray-200 rounded-full duration-400 hover:bg-transparent">
             #{info.tag3}
           </span>
-        ) : (
-          ""
         )}
       </div>
     </div>
