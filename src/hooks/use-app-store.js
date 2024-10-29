b6f7f7198c6fe6ef1f3c1f5464000e3c1fbc40b5
emiliano lucero <emilianoglucero@gmail.com>
2024-10-28 23:52:35 -0300

    Refactor useAppStore hook and global CSS
    - Removed useAppStore hook as it is no longer needed
    - Updated global CSS to remove commented out code and use custom color variable for selection background

--- src/hooks/use-app-store.js
+++
@@ -1,6 +0,0 @@
-import { create } from "zustand"
-
-export const useAppStore = create(set => ({
-  isIntroComplete: false,
-  setIsIntroComplete: isComplete => set({ isIntroComplete: isComplete }),
-}))
