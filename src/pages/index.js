2f656486de28783ef5cabfea132048467f7d2bec
emiliano lucero <emilianoglucero@gmail.com>
2024-11-06 00:11:58 -0300

    - conditionally render p5 background in mobile devices

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,8 +8,10 @@ import useIsomorphicLayoutEffect from "../hooks/use-isomorphic-layout-effect"
 import SEO from "../components/SEO/SEO"
 import IntroLoader from "../components/Loader/IntroLoader"
 import P5 from "../p5/p5"
+import { useDeviceDetect } from "../hooks/use-device-detect"
 
 export default function Home() {
+  const { isMobile } = useDeviceDetect()
   useIsomorphicLayoutEffect(() => {
     const animateMainContainer = () => {
       gsap.to(".main-container", {
@@ -37,10 +39,10 @@ export default function Home() {
   }, [])
 
   return (
-    <div className="min-h-screen bg-white">
-      <div className="fixed w-full h-full bg-white opacity-0 main-container">
+    <div className="min-h-screen">
+      <div className="fixed w-full h-full opacity-0 main-container">
         <SEO />
-        <P5 />
+        {!isMobile && <P5 />}
         <div className="container px-6 mx-auto text-4xl text-center font-nb_international_pro my-14 md:px-12 lg:px-12 text-primary">
           <Header />
           <div className="grid content-center justify-center grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-16 lg:grid-rows-1">
